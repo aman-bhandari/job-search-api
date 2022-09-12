15aa08a641933bf66bb48f6642b8db2df91b582c
Aman <bhandari.aman0101@gmail.com>
2022-09-12 15:26:05 +0530

    User created in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,8 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
 const register = async (req, res) => {
-  res.send('user registered')
+  const user = await User.create(req.body)
+  res.status(StatusCodes.CREATED).json(user)
 }
 const logIn = async (req, res) => {
   res.send('user logged in')
