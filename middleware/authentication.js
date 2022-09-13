e7cea544c33692e4b0316d9367298cf291a34aca
Aman <bhandari.aman0101@gmail.com>
2022-09-13 16:01:16 +0530

    authentication middleware setup

---
+++ middleware/authentication.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
+
+const auth = async (req, res, next) => {
+  //check for header
+
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authentication Invalid')
+  }
+
+  try {
+    const token = authHeader.split(' ')[1]
+    const payLoad = jwt.verify(token, process.env.JWT_SECRET)
+    console.log(payLoad)
+    req.user = { userID: payLoad.userID, name: payLoad.name }
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication Invalid')
+  }
+}
+
+module.exports = auth
