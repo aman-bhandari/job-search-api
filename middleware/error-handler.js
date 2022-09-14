0b41274dd2b7e84705310e93c5cff7b3189d04fb
Aman <bhandari.aman0101@gmail.com>
2022-09-14 13:32:52 +0530

    Added validation error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -9,7 +9,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // }
-
+  if (err.name === 'ValidationError') {
+    ;(customError.statusCode = 400),
+      (customError.message = Object.values(err.errors)
+        .map((item) => item.message)
+        .join(', '))
+  }
   if (err.code && err.code === 11000) {
     ;(customError.statusCode = 400),
       (customError.message = `Duplicate value found for ${Object.keys(
